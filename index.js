5e51124a7b166eb01e9bf157dd6c40af6f66e4a3
zafergursoy82 <zafergursoyy1982@gmail.com>
2025-08-30 15:04:23 +0300

    update

--- index.js
+++ index.js
@@ -6,13 +6,25 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
-const d = new Date();
+const today = new Date("July 26, 2025 01:15:00");
+// const today = new Date("");
 const WeekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const dayOfWeek = WeekDays[d.getDay()];
+
+let dayNamee = WeekDays[today.getDay()];
+let dayTypee = "a weekday";
+let advicee = "it's time to work hard !! :(";
+
+if(dayNamee === "Saturday" || dayNamee === "Sunday") {
+  dayTypee = "the weekend";
+  advicee = "Just relax. it's time to have fun :)";
+}
 
 app.get("/", (req, res) => {
-  res.render("index.ejs",
-    { dayOfWeek: dayOfWeek }
+  res.render("index.ejs", {
+     dayName: dayNamee,
+     dayType: dayTypee,
+     advice: advicee
+    }
   );
 });
 
